4fa67150ced497ba6937d769dd4daf1e7ef7a3ca
striado <s.triado@sapalomera.cat>
2022-10-01 14:06:53 +0200

    bbdd script add

---
+++ script_bbdd/pt02_sergi_triado.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-10-2022 a las 14:03:16
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pt02_sergi_triado`
+--
+
+DROP DATABASE IF EXISTS `pt02_sergi_triado`;
+
+CREATE DATABASE IF NOT EXISTS `pt02_sergi_triado;`;
+
+USE `pt02_sergi_triado`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuaris`
+--
+
+CREATE TABLE `usuaris` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `dni` varchar(9) NOT NULL,
+  `adresa` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuaris`
+--
+
+INSERT INTO `usuaris` (`id`, `nom`, `dni`, `adresa`) VALUES
+(1, 'SERGI', '12345678L', 'example@example.com'),
+(3, 'JOAN', '12435867P', 'papapap@gmail.com'),
+(5, 'Sergi', '99999999P', 'aaa@aaa.com'),
+(7, 'PAPO', 'AAAA', 'sasa@sasa.com'),
+(9, 'XAVI', '43223411F', 'yiyoyo@momo.net'),
+(10, 'XAVI', '32112367Y', 'tutit@aaa.es'),
+(13, 'NOU', 'ASDFA', 'qqqq@qqq.qq'),
+(14, 'PROVA', 'AWIEOAIWE', 'qpweoiqp@wq.asa'),
+(15, 'PROVO', 'ALSKJD', 'awoepiq@qweq.com'),
+(17, 'PARAVA', 'RESERQWE', 'ererere@rere.com'),
+(19, 'POPOPO', 'WERWERWE', 'qweqweq@qweq.cat'),
+(21, 'CLARA', '928349283', 'grgrrg@grrr.gr'),
+(22, 'MARTA', '12391', 'pprropo@qwpeoq.cat'),
+(24, 'OSCAR', '0912381', 'trieot@qwoepiq.net'),
+(26, 'PRIMO', '1329481', 'qweqw@popoa.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuaris`
+--
+ALTER TABLE `usuaris`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`,`adresa`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuaris`
+--
+ALTER TABLE `usuaris`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
